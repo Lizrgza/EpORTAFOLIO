feaab0214f78db9c0bbe6e4e8620f33c9c97a5f0
lizbethra <lizbethra76@gmail.com>
2023-05-30 18:50:04 -0700

    Vistas Cliente / inicio/portafolio/acercademi

---
+++ EpORTAFOLIO/Controllers/PortafolioController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using EpORTAFOLIO.Models;
+
+namespace EpORTAFOLIO.Controllers
+{
+    public class PortafolioController : Controller
+    {
+        // GET: Portafolio
+        public ActionResult VistaIndex()
+        {
+            return View();
+        }
+    }
+}
